fe8f633b9b099956a3281cef740e9aa9b3c6197c
User <user@Users-MacBook-Pro.local>
2019-03-13 18:45:16 -0500

    skeleton

---
+++ javascript/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+    // function for user interaction in search bar //
+
+    // function for button when interacting with search button //
+    
+    // function for ajax call to currency api //
+
+    
+    // function for ajax call to image api //
+
+
+    // function for ajax call to yelp api //
+
+
+
+
+});
