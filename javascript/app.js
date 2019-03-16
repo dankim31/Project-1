bb8e8a4e145b21856bb6dc3e4951a459d2a7b89c
YinshengGuo <yinshengguo@gmail.com>
2019-03-16 11:04:08 -0500

    init integration setup

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,15 @@
-$(document).ready(function() {
+console.log('hello');
 
-    // function for user interaction in search bar //
+// $(document).ready(function() {
+//     console.log('hello');
 
-    // function for button when interacting with search button //
+//     // function for user interaction in search bar //
+
+//     // function for button when interacting with search button //
     
-    // function for ajax call to currency api //
-
+//     // function for ajax call to currency api //
     
-    // function for ajax call to image api //
+//     // function for ajax call to image api //
 
-
-    // function for ajax call to yelp api //
-
-
-
-
-});
+//     // function for ajax call to yelp api //
+// });
